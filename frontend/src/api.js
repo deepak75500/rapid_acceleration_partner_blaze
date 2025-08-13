0ec15f3ed3b233f79d47884df3f5b7e09132be65
deepak75500 <162658459+deepak75500@users.noreply.github.com>
2025-08-13 16:35:18 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Use environment variable if set, otherwise fallback to Render backend
-const API = import.meta.env.VITE_API_URL || 'https://rapid-acceleration-partner-blaze.onrender.com';
+const API =  'https://rapid-acceleration-partner-blaze.onrender.com';
 
 const instance = axios.create({
   baseURL: API,
